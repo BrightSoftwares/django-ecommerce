123fd9860fbb3b7abcdc605a9d925bea6a3b9490
Full Bright <full3right@gmail.com>
2020-07-19 12:48:50 +0200

    setup celery

--- djecommerce/celery.py
+++ djecommerce/celery.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, unicode_literals
+from django.conf import settings
 
 import os
 
@@ -8,16 +9,16 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'djecommerce.settings.development')
 
-app = Celery('djecommerce')
+app = Celery('djecommerce', broker=settings.CELERY_BROKER_URL)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
