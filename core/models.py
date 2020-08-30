d51d7fee8a57cffa14f67ebd113538ff76e240e2
Full Bright <full3right@gmail.com>
2020-08-30 14:15:46 +0200

    Add shipment method

--- core/models.py
+++ core/models.py
@@ -5,20 +5,6 @@ from django.db.models import Sum
 from django.shortcuts import reverse
 from django_countries.fields import CountryField
 
-# TODO : Make this an object
-# CATEGORY_CHOICES = (
-#     ('S', 'Shirt'),
-#     ('SW', 'Sport wear'),
-#     ('OW', 'Outwear')
-# )
-
-# TODO : Make this an object
-# LABEL_CHOICES = (
-#     ('P', 'primary'),
-#     ('S', 'secondary'),
-#     ('D', 'danger')
-# )
-
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
@@ -39,8 +25,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 class Label(models.Model):
+    name = models.CharField(max_length=256)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ShippingMethod(models.Model):
     name = models.CharField(max_length=256)
     date_created = models.DateTimeField(auto_now_add=True)
 
@@ -84,36 +81,11 @@ class Item(models.Model):
         ordering = ['-id']
 
 
-class OrderItem(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
-    ordered = models.BooleanField(default=False)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-
-    def __str__(self):
-        return f"Product: {self.item.title}, Qty: {self.quantity} for {self.user.username}"
-
-    def get_total_item_price(self):
-        return self.quantity * self.item.price
-
-    def get_total_discount_item_price(self):
-        return self.quantity * self.item.discount_price
-
-    def get_amount_saved(self):
-        return self.get_total_item_price() - self.get_total_discount_item_price()
-
-    def get_final_price(self):
-        if self.item.discount_price:
-            return self.get_total_discount_item_price()
-        return self.get_total_item_price()
-
-
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     ref_code = models.CharField(max_length=20, blank=True, null=True)
-    items = models.ManyToManyField(OrderItem)
+    # items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
@@ -125,6 +97,8 @@ class Order(models.Model):
         'Payment', on_delete=models.SET_NULL, blank=True, null=True)
     coupon = models.ForeignKey(
         'Coupon', on_delete=models.SET_NULL, blank=True, null=True)
+    shipping_method = models.ForeignKey(
+        'ShippingMethod', on_delete=models.PROTECT, null=True)
     being_delivered = models.BooleanField(default=False)
     received = models.BooleanField(default=False)
     refund_requested = models.BooleanField(default=False)
@@ -152,6 +126,33 @@ class Order(models.Model):
         if self.coupon:
             total -= self.coupon.amount
         return total
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="items", related_query_name="orderitem", null=True)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"Product: {self.item.title}, Qty: {self.quantity} for {self.user.username}"
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def get_total_discount_item_price(self):
+        return self.quantity * self.item.discount_price
+
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_total_discount_item_price()
+
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
 
 
 ADDRESS_CHOICES = (
